44231c198011944b824499a52c49cd6ec226c696
AhmedAyoub5757 <136374743+AhmedAyoub5757@users.noreply.github.com>
2024-01-09 15:47:45 +0500

    Add files via upload

---
+++ Task04/script.js
@@ -0,0 +1,38 @@
+var slideIndex = 1;
+
+function openModal() {
+    document.getElementById('modal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName('slide');
+    var modal = document.getElementById('modal');
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+    modal.style.display = 'block';
+}
+
